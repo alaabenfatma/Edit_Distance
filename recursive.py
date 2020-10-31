237c1279d6f35dc89b80d057f62d5c91fb2e420c
Alaa <9027148+alaabenfatma@users.noreply.github.com>
2020-10-31 03:45:10 +0100

    naive_recursive_approach

--- recursive.py
+++ recursive.py
@@ -1,6 +1,22 @@
 import generator
 
-str1 = generator.randomword(10)
-str2 = generator.randomword(10)
+str1 = generator.randomword(5)
+str2 = generator.randomword(5)
+print(f'Calculating edit distance between "{str1}" and "{str2}"')
 
-print(str1,str2)
+# replace can cost either 1 (insert a character on top of another) or 2 (remove a character and then insert the new one)
+replace_cost = 1
+
+
+def ed(s1, s2, n, m):
+    if(n == 0):
+        return m
+    if(m == 0):
+        return n
+    if(s1[n-1] == s2[m-1]):
+        return ed(s1, s2, n-1, m-1)
+    else:
+        return 1 + min(ed(s1, s2, n-1, m), ed(s1, s2, n, m-1), ed(s1, s2, n-1, m-1)+replace_cost)
+
+
+print(f'The edit distance equals: {ed(str1, str2, len(str1), len(str2))}')
