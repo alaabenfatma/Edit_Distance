3c453617b90ca9d1efadb8eaba3856e8a729091a
Claire Robin <43317509+Clair3@users.noreply.github.com>
2020-11-02 01:36:54 +0100

    Update recursive.py

--- recursive.py
+++ recursive.py
@@ -5,7 +5,7 @@ str2 = generator.randomword(5)
 print(f'Calculating edit distance between "{str1}" and "{str2}"')
 
 # replace can cost either 1 (insert a character on top of another) or 2 (remove a character and then insert the new one)
-replace_cost = 1
+replace_add_cost = 0 #case where replace is 1 operation (= 1 otherwise)
 
 
 def ed(s1, s2, n, m):
@@ -16,7 +16,7 @@ def ed(s1, s2, n, m):
     if(s1[n-1] == s2[m-1]):
         return ed(s1, s2, n-1, m-1)
     else:
-        return 1 + min(ed(s1, s2, n-1, m), ed(s1, s2, n, m-1), ed(s1, s2, n-1, m-1)+replace_cost)
+        return 1 + min(ed(s1, s2, n-1, m), ed(s1, s2, n, m-1), ed(s1, s2, n-1, m-1)+replace_add_cost)
 
 
 print(f'The edit distance equals: {ed(str1, str2, len(str1), len(str2))}')
