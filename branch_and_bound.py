c39e487d928ac43b0a78f6c5537c9085925b6c24
Alaa <9027148+alaabenfatma@users.noreply.github.com>
2020-11-05 06:56:21 +0100

    finished a working version of the branch_and_bound algo

---
+++ branch_and_bound.py
@@ -0,0 +1,42 @@
+'''
+Things to keep in mind: 
+    - lowerBound will NEVER surpass the length of the longest string
+    - if the currentDistance reaches, somehow, the length of the longest string, we break out of the function.
+'''
+from generator import randomword
+
+str1 = 'x'
+str2 = 'x'
+
+i = 0
+
+
+def bounded_ed(a, b, currentDistance, lowerBound):
+    """
+    Edit distance - but bounded.
+    """
+    global i
+    i += 1
+    n = len(a)
+    m = len(b)
+    if(a == b):
+        return currentDistance
+    if currentDistance >= lowerBound:
+        return lowerBound
+    if (n == 0):
+        return m + currentDistance
+    if (m == 0):
+        return n + currentDistance
+
+    replace = 0 if (a[-1] == b[-1]) else 1
+
+    insertionBranch = bounded_ed(a[:n - 1], b, currentDistance + 1, lowerBound)
+    deletionBranch = bounded_ed(a, b[:m - 1], currentDistance + 1,
+                                min(insertionBranch, lowerBound))
+    replaceBranch = bounded_ed(
+        a[:n - 1], b[:m - 1], currentDistance + replace,
+        min(insertionBranch, deletionBranch, lowerBound))
+    return min(insertionBranch, deletionBranch, replaceBranch)
+
+
+print(bounded_ed(str1, str2, 0, max(len(str1), len(str2))), i)
