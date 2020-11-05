a8b6f29b3710c17515d0fd116dd5e64807d24aeb
Alaa <9027148+alaabenfatma@users.noreply.github.com>
2020-11-05 06:55:45 +0100

    tuned and enhanced the algorithm

--- recursive.py
+++ recursive.py
@@ -1,22 +1,30 @@
 import generator
 
-str1 = generator.randomword(5)
-str2 = generator.randomword(5)
+str1 = 'x'
+str2 = 'x'
 print(f'Calculating edit distance between "{str1}" and "{str2}"')
 
 # replace can cost either 1 (insert a character on top of another) or 2 (remove a character and then insert the new one)
-replace_add_cost = 0    # case where replace is 1 operation (= 1 otherwise)
+replace_add_cost = 0  # case where replace is 1 operation (= 1 otherwise)
+i = 0
 
 
 def ed(s1, s2, n, m):
-    if(n == 0):
+    global i
+    i += 1
+    if(s1 == s2):
+        return 0
+    if (n == 0):
         return m
-    if(m == 0):
+    if (m == 0):
         return n
-    if(s1[n-1] == s2[m-1]):
-        return ed(s1, s2, n-1, m-1)
+    if (s1[n - 1] == s2[m - 1]):
+        return ed(s1, s2, n - 1, m - 1)
     else:
-        return 1 + min(ed(s1, s2, n-1, m), ed(s1, s2, n, m-1), ed(s1, s2, n-1, m-1)+replace_add_cost)
+        insertionBranch = ed(s1, s2, n - 1, m)
+        deletionBranch = ed(s1, s2, n, m - 1)
+        replaceBranch = ed(s1, s2, n - 1, m - 1) + replace_add_cost
+        return 1 + min(insertionBranch, deletionBranch, replaceBranch)
 
 
-print(f'The edit distance equals: {ed(str1, str2, len(str1), len(str2))}')
+print(f'The edit distance equals: {ed(str1, str2, len(str1), len(str2))}', i)
