96dcddd55b1146f553ca92d6b7709d0041b7b0b7
Claire Robin <43317509+Clair3@users.noreply.github.com>
2020-11-27 16:41:24 +0100

    Add files via upload

---
+++ branch_and_bound.py
@@ -0,0 +1,104 @@
+'''
+Things to keep in mind:
+    - lowerBound will NEVER surpass the length of the longest string
+    - if the currentDistance reaches, somehow, the length of the longest string, we break out of the function.
+'''
+'''
+i = 0
+
+k = max(len(str1), len(str2))'''
+'''
+-the first 3 table are here for the sake of understanding the problem
+-the idea was to store in different tables each ind of operation
+-to see what choice has been made or could have been made at that bound or node
+-the last table named table is for storing the operations that have been exactly with the algo
+''''''
+insert_table = [0] * k
+delete_table = [0] * k
+replace_table = [0] * k
+table = [0] * k #the table where i will store the operation chosen at each bound (node)'''
+
+def bounded_ed(a, b, currentDistance, lowerBound, insert_table, delete_table, replace_table, table, i):
+    """
+    Edit distance - but bounded.
+    """
+    i += 1
+    n = len(a)
+    m = len(b)
+
+    if (a == b):
+        # update the operations table
+        return currentDistance
+
+    if currentDistance >= lowerBound:
+        # update the operations table
+        return lowerBound
+
+    if (n == 0):
+        # update the operations table
+        return m + currentDistance
+    if (m == 0):
+        return n + currentDistance
+    if (a[-1] == b[-1]):
+        # update the operations table
+
+        return bounded_ed(a[:- 1], b[:- 1], currentDistance, lowerBound, insert_table, delete_table, replace_table, table, i)
+
+
+    else:
+
+        insertionBranch = bounded_ed(a[:n - 1], b, currentDistance + 1, lowerBound, insert_table, delete_table, replace_table, table, i)
+        deletionBranch = bounded_ed(a, b[:m - 1], currentDistance + 1,
+                                    min(insertionBranch, lowerBound), insert_table, delete_table, replace_table, table, i)
+        replaceBranch = bounded_ed(
+            a[:n - 1], b[:m - 1], currentDistance + 1,
+            min(insertionBranch, deletionBranch, lowerBound), insert_table, delete_table, replace_table, table, i)
+
+        '''
+        as we need to find what branch will minimize our cost,
+        we need to store that operation before calling next recursion
+        that's what is done here
+        '''
+        if (min(insertionBranch, deletionBranch, replaceBranch) == insertionBranch):
+            table[currentDistance] = 1  # 1 is for encoding the insertion operation
+            insert_table[currentDistance] = 1
+        elif (min(insertionBranch, deletionBranch, replaceBranch) == deletionBranch):
+            delete_table[currentDistance] = 1
+            table[currentDistance] = 2  # 2 is for encoding the deletion operation
+        elif (min(insertionBranch, deletionBranch, replaceBranch) == replaceBranch):
+            replace_table[currentDistance] = 1
+            table[currentDistance] = 3  # 3 is for encoding the replacement operation
+
+    return min(insertionBranch, deletionBranch, replaceBranch)
+
+
+def branch_and_bound(str1, str2):
+    i = 0
+    k = max(len(str1), len(str2))
+    '''
+    -the first 3 table are here for the sake of understanding the problem
+    -the idea was to store in different tables each ind of operation
+    -to see what choice has been made or could have been made at that bound or node
+    -the last table named table is for storing the operations that have been exactly with the algo
+    '''
+
+    insert_table = [0] * k
+    delete_table = [0] * k
+    replace_table = [0] * k
+    table = [0] * k  # the table where i will store the operation chosen at each bound (node)
+
+    ed = bounded_ed(str1, str2, 0, k, insert_table, delete_table, replace_table, table, i)
+    '''Since the table stored the operation in a recursive way
+       We need to reverse the printage of the table to read from left to right
+       We can keep it as it is but rememberif you do that when reading you should start from right to left
+    '''
+    alignment = list(reversed(table))
+    return ed, alignment
+
+
+
+
+if __name__ == '__main__':
+    str1 = 'cat'
+    str2 = 'carpet'
+    print(branch_and_bound(str1, str2))
---
+++ evaluation_with_generator.py
@@ -0,0 +1,87 @@
+import sys
+import random
+import string
+import numpy as np
+import time
+import matplotlib.pyplot as plt
+
+import dynamic_programming_ED as dp
+import naive_recursive as re
+import branch_and_bound as bb
+import divide_and_conquer as dc
+import greedy_approach as ga
+import k_stripes_dynamic_programming as kdp
+
+
+
+def randomword(n=0):
+    '''
+    A function to generate a random string of length n.
+    '''
+    return ''.join(random.choice(string.ascii_lowercase) for i in range(n))
+
+def randomword_alphabet(n,alphabet):
+    return ''.join(random.choices(alphabet, k=n))
+
+#Time evaluation
+#ED evaluation
+#alignment evaluation
+
+def evaluation_lenght_variation(n):
+    step = 4
+    K = 0
+    T = int(n / step)
+    tab_time = np.zeros((T, 6))
+    absysse = np.zeros(T)
+    for i in range(step, n+1, step):
+        absysse[K] = i
+        tab_time_j = np.zeros((6, 3))
+        for j in range(3):
+            print('j', j)
+            string1, string2 = randomword(i), randomword(i)
+            curr_time = time.time()
+            ED1, alignement = dp.dynamic_programming(string1, string2)
+            tab_time_j[0][j] = time.time() - curr_time
+            curr_time = time.time()
+            ED2, alignement = kdp.K_stripes_DP_ED(string1, string2, 3)
+            tab_time_j[1][j] = time.time() - curr_time
+            curr_time = time.time()
+            #ED3, alignement = re.recursive(string1, string2)
+            #tab_time_j[2][j] = time.time() - curr_time
+            curr_time = time.time()
+            ED4 = ga.greedy_approach(string1, string2)
+            tab_time_j[3][j] = time.time() - curr_time
+            curr_time = time.time()
+            ED5, alignement = bb.branch_and_bound(string1, string2)
+            tab_time_j[4][j] = time.time() - curr_time
+            #curr_time = time.time()
+            #ED divide and conquer"""
+            print(ED1, ED2, ED4, ED5)
+        tab_time[K] = np.mean(tab_time_j, axis=1)
+        K += 1
+
+    plt.plot(absysse, tab_time)
+    plt.yscale("log")
+    #plt.legend(('Dynamic', 'K stripes DP', 'Greedy', 'Branch and Bound'), loc=0)
+    plt.legend(('Dynamic', 'K stripes DP', 'Recursive', 'Greedy', 'Branch and Bound', 'Divide and Conquer'), loc=0)
+    plt.title("time evaluation according to word length")
+    plt.savefig('time_eval_n=10.png')
+    plt.show()
+    plt.close()
+
+
+    return
+
+if __name__ == '__main__':
+    '''
+    A = np.array([[1, 1, 1], [2, 2, 2], [5, 10, 15]])
+    tab_time = np.zeros((2, 3))
+    t
+    plt.plot(A.T)
+    plt.legend(('Total Cost', 'Construction Cost', 'Gas Cost', 'Wage Cost'), loc=0)
+    plt.show()
+    plt.close()'''
+    evaluation_lenght_variation(20)
+
+
+
