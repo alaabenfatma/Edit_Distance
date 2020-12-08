95bc45c74de9d59a7150b2ce2f186e4653cdc4d0
Claire Robin <43317509+Clair3@users.noreply.github.com>
2020-12-08 15:58:15 +0100

    Add files via upload

--- evaluation_with_generator.py
+++ evaluation_with_generator.py
@@ -4,11 +4,10 @@ import string
 import numpy as np
 import time
 import matplotlib.pyplot as plt
-
+import divide_and_conquer as dc
 import dynamic_programming_ED as dp
 import naive_recursive as re
 import branch_and_bound as bb
-import divide_and_conquer as dc
 import greedy_approach as ga
 import k_stripes_dynamic_programming as kdp
 
@@ -28,14 +27,14 @@ def randomword_alphabet(n,alphabet):
 #alignment evaluation
 
 def evaluation_lenght_variation(n):
-    step = 4
+    step = 2
     K = 0
     T = int(n / step)
-    tab_time = np.zeros((T, 6))
+    tab_time = np.zeros((T, 3))
     absysse = np.zeros(T)
     for i in range(step, n+1, step):
         absysse[K] = i
-        tab_time_j = np.zeros((6, 3))
+        tab_time_j = np.zeros((3, 3))
         for j in range(3):
             print('j', j)
             string1, string2 = randomword(i), randomword(i)
@@ -45,43 +44,43 @@ def evaluation_lenght_variation(n):
             curr_time = time.time()
             ED2, alignement = kdp.K_stripes_DP_ED(string1, string2, 3)
             tab_time_j[1][j] = time.time() - curr_time
-            curr_time = time.time()
-            #ED3, alignement = re.recursive(string1, string2)
-            #tab_time_j[2][j] = time.time() - curr_time
-            curr_time = time.time()
-            ED4 = ga.greedy_approach(string1, string2)
-            tab_time_j[3][j] = time.time() - curr_time
+            #if i < 10:
+            """curr_time = time.time()
+            ED3, alignement = re.recursive(string1, string2)
+            tab_time_j[2][j] = time.time() - curr_time
             curr_time = time.time()
             ED5, alignement = bb.branch_and_bound(string1, string2)
             tab_time_j[4][j] = time.time() - curr_time
-            #curr_time = time.time()
-            #ED divide and conquer"""
-            print(ED1, ED2, ED4, ED5)
+            curr_time = time.time()
+            ED4, alignement = ga.greedy_approach(string1, string2)
+            tab_time_j[3][j] = time.time() - curr_time"""
+            curr_time = time.time()
+            ED6, alignement = dc.divide_and_conquer(string1, string2)
+            tab_time_j[2][j] = time.time() - curr_time
+            print(ED1, ED2, ED6)
         tab_time[K] = np.mean(tab_time_j, axis=1)
         K += 1
 
     plt.plot(absysse, tab_time)
-    plt.yscale("log")
+    #plt.yscale("log")
     #plt.legend(('Dynamic', 'K stripes DP', 'Greedy', 'Branch and Bound'), loc=0)
-    plt.legend(('Dynamic', 'K stripes DP', 'Recursive', 'Greedy', 'Branch and Bound', 'Divide and Conquer'), loc=0)
+    plt.legend(('Dynamic', 'K stripes DP', 'Divide and Conquer'), loc=0)
     plt.title("time evaluation according to word length")
-    plt.savefig('time_eval_n=10.png')
+    plt.savefig('time_eval_n=20.png')
     plt.show()
     plt.close()
 
 
     return
 
+def test():
+    a = 4221000
+    b = 46.5
+    return 19359753 / (a/b)
+
+
 if __name__ == '__main__':
-    '''
-    A = np.array([[1, 1, 1], [2, 2, 2], [5, 10, 15]])
-    tab_time = np.zeros((2, 3))
-    t
-    plt.plot(A.T)
-    plt.legend(('Total Cost', 'Construction Cost', 'Gas Cost', 'Wage Cost'), loc=0)
-    plt.show()
-    plt.close()'''
-    evaluation_lenght_variation(20)
+    print(259.8 / 24)
 
 
 
